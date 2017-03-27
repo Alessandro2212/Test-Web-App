91338b031c278014e9971d70c7717896f0d2413a
Alessandro2212 <alessandro.zelli1987@gmail.com>
2017-03-28 00:54:24 +0200

    javascipt

--- AstAppWebApi/Scripts/Login/TestLogin.js
+++ AstAppWebApi/Scripts/Login/TestLogin.js
@@ -1,26 +1,30 @@
-﻿function LoadtaskTableData() {
-    $.ajax({
-        url: _taskListAddress,
-        type: 'GET',
-        data: {},
-        success: function (data) {
-            try {
+﻿//function LoadtaskTableData() {
+//    $.ajax({
+//        url: _taskListAddress,
+//        type: 'GET',
+//        data: {},
+//        success: function (data) {
+//            try {
 
-                if (isToRefresh) {
-                    RefreshDatatable('#dt_show', data);
-                } else {
-                    SetDataTableWithoutAjax('#dt_show', data, _taskTableColumnTemplate,
-                    _taskTableColumnsDefs, true, 0, "asc");
-                }
+//                if (isToRefresh) {
+//                    RefreshDatatable('#dt_show', data);
+//                } else {
+//                    SetDataTableWithoutAjax('#dt_show', data, _taskTableColumnTemplate,
+//                    _taskTableColumnsDefs, true, 0, "asc");
+//                }
 
-                SetPageNumber('#dt_show', parseInt(_taskCurrentPageNumber));
+//                SetPageNumber('#dt_show', parseInt(_taskCurrentPageNumber));
 
-            } catch (err) {
-                UIkit.modal.alert("Error on success Load Job Table: " + err.message);
-            }
-        },
-        error: function (error) {
-            UIkit.modal.alert("Error on error: " + error.message);
-        }
-    });
+//            } catch (err) {
+//                UIkit.modal.alert("Error on success Load Job Table: " + err.message);
+//            }
+//        },
+//        error: function (error) {
+//            UIkit.modal.alert("Error on error: " + error.message);
+//        }
+//    });
+//}
+
+function LoginPerformed(data) {
+    var accessToken  = data.access_token
 }
